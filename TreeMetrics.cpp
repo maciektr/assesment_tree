86fbe0b43745fd2f4ed60190500e37d61893dcf1
Maciej <maciek2tr99@gmail.com>
2020-03-09 22:50:12 +0100

    Add memorization to previously implemented functions

--- TreeMetrics.cpp
+++ TreeMetrics.cpp
@@ -7,13 +7,21 @@ double TreeMetrics::average(Node *node){
 int TreeMetrics::count_elements(Node *node){
     if(node == nullptr)
         return 0;
-    return 1 + TreeMetrics::count_elements(node->left) + TreeMetrics::count_elements(node->right);
+
+    if(this->mem_count.find(node) == this->mem_count.end())        
+        this->mem_count[node] = (1 + TreeMetrics::count_elements(node->left) + TreeMetrics::count_elements(node->right));
+
+    return this->mem_count[node];
 }
 
 int TreeMetrics::sum(Node *node){
     if(node == nullptr)
         return 0;
-    return node->data + TreeMetrics::sum(node->left) + TreeMetrics::sum(node->right);
+
+    if(this->mem_sum.find(node) == this->mem_sum.end())        
+        this->mem_sum[node] = (node->data + TreeMetrics::sum(node->left) + TreeMetrics::sum(node->right));
+
+    return this->mem_sum[node];
 }
 
 void TreeMetrics::_median(Node *node, TreeMetrics::pque_max &smaller, TreeMetrics::pque_min &larger){
